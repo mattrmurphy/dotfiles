a3d3e39d49a5bd3dfac38606e63257604eddcc5d
Matt Murphy <mmurphydev@gmail.com>
2025-05-07 16:23:41 -0400

    switch to ts_ls for js and ts LSP

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,10 +1,23 @@
-require'lspconfig'.tsserver.setup{
-    on_attach = function()
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer=0})
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer=0})
-	vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, {buffer=0})
-    end,
-}
+require'lspconfig'.ts_ls.setup{}
+vim.lsp.config('ts_ls', {
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+        languages = {"javascript", "typescript", "vue"},
+      },
+    },
+  },
+  filetypes = {
+    "javascript",
+    "typescript",
+    "vue",
+  },
+})
+
+vim.lsp.enable('ts_ls')
+
 require'lspconfig'.gopls.setup{
     on_attach = function()
 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer=0})
@@ -22,3 +35,9 @@ require'lspconfig'.gopls.setup{
     },
   },
 }
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+})
